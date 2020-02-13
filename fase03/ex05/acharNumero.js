9f29e7489e12077bb1c616385943ed00938affa6
camilagomide <57682186+camilagomide@users.noreply.github.com>
2020-02-13 14:36:33 +0100

    numero metade

---
+++ fase03/ex05/acharNumero.js
@@ -0,0 +1,14 @@
+function acharNumero(numero){
+if (5> numero){
+    return "mercurio"
+}
+if (5<=numero && numero>=10){
+    return "venus"
+}
+if (10 <=numero && numero>=100){
+    return "terra"
+}
+if(100< numero){
+    return "marte"
+}
+}
