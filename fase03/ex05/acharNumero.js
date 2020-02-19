a99f4fef48257577dab27f3ceeefdaf77af1af4f
camilagomide <57682186+camilagomide@users.noreply.github.com>
2020-02-19 17:45:02 +0100

    Onde está meu numero

--- fase03/ex05/acharNumero.js
+++ fase03/ex05/acharNumero.js
@@ -1,11 +1,12 @@
 function acharNumero(numero){
-if (5> numero){
+    var n= numero 
+if (5>numero){
     return "mercurio"
 }
-if (5<=numero && numero>=10){
+if (5<= numero && numero <=10){
     return "venus"
 }
-if (10 <=numero && numero>=100){
+if (10<numero && numero <=100){
     return "terra"
 }
 if(100< numero){
