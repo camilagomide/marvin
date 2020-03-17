85346475150c1e50baa400b77ae5eea1c3799434
camilagomide <57682186+camilagomide@users.noreply.github.com>
2020-03-17 15:35:22 +0100

    tempo

--- fase03/ex00/parOuImpar.js
+++ fase03/ex00/parOuImpar.js
@@ -1,9 +1,13 @@
-function nove(numero){
-    var n=numero.toString();  
-    if (n.substring(n.length-1) == 9){
-        return true;
-    }
-    else
-        return false
-    
+function tempo(data1, data2,unidade){
+var resultado= new Date (data2)- new Date (data1);
+if (unidade=== "ano") {
+    resultado = (new Date (data2) - new Date (data1))/(1000*60*60*24*30*12);
+}
+else if (unidade=== "mês") {
+    resultado = (new Date (data2)- new Date (data1))/(1000*60*60*24*30);
+} 
+else if (unidade=== "dia") {
+    resultado = (new Date (data2)- new Date (data1))/ (1000*60*60*24);
+}
+return Math.round(Math.abs(resultado))
 }
