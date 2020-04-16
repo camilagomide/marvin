7e0b34b2b05faf1e2fd99b93f27501341dd71ec0
camilagomide <57682186+camilagomide@users.noreply.github.com>
2020-04-16 12:54:30 +0200

    fizzbuzz

---
+++ fase04/ex02/fizzbuzz.js
@@ -0,0 +1,11 @@
+function fizzbuzz(num1, num2){
+    var i= num1;
+    var f= num2;
+    while (i<=f){
+        console.log(i);
+        i++
+    }
+    if(i++%3===0){
+        return "fizz"
+    }
+    }
