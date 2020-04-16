e15e4e58a6ddc941cdd3db8c537ce3c0cfd5e09c
camilagomide <57682186+camilagomide@users.noreply.github.com>
2020-04-16 12:57:50 +0200

    entreNumeros

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,16 +1,9 @@
-function ateZero(num){
-    var n=num;
-    var i= 0;
-if (n>0){
-    while(i<=n){
-    console.log (i);
-    i++;
-}
-if (n<0){
-    while (n<=i){
-        console.log (n);
-        n++;
+function entreNumeros (min,max){
+    var i= min
+    var f= max
+    while (i<=f){
+        console.log(i);
+        i++
     }
-}
-}
+    
 }
