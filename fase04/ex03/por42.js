ce4e1008160e5311b706c0656c3300219f203529
camilagomide <57682186+camilagomide@users.noreply.github.com>
2020-06-01 23:49:52 +0200

    por42

--- fase04/ex03/por42.js
+++ fase04/ex03/por42.js
@@ -3,9 +3,11 @@ function por42(num1, num2){
     var multiplica= 1; 
     var achou= true; 
 while(achou){
-    var resultado= numero*multiplica; 
-    if (resultado >= num1 & resultado<=num2) {
-        multiplica ++
+    var resultado= numero * multiplica; 
+    if (resultado >= num1 & resultado <= num2) {
+        multiplica ++;
+     resultado=numero+multiplica;
+        achou=false
     }
     else {
         if (resultado > num2) {
