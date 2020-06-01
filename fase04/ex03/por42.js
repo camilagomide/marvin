204d6be2ca0133915db41912269a5020c1a0bcc7
camilagomide <57682186+camilagomide@users.noreply.github.com>
2020-06-01 21:45:15 +0200

    por42

---
+++ fase04/ex03/por42.js
@@ -0,0 +1,20 @@
+function por42(num1, num2){
+    var numero= 42;
+    var multiplica= 1; 
+    var achou= true; 
+while(achou){
+    var resultado= numero*multiplica; 
+    if (resultado >= num1 & resultado<=num2) {
+        multiplica ++
+    }
+    else {
+        if (resultado > num2) {
+            achou= false
+            resultado= "Não encontrado";
+        }
+    else {
+    multiplica ++
+    }
+}
+}
+}
