f651daab6a7deaca13bd46e27267af1579751ef1
camilagomide <57682186+camilagomide@users.noreply.github.com>
2020-06-15 19:21:24 +0200

    entrenumeros

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,15 +1,8 @@
-function por42(num1, num2){
-    var multiplo=42;
-    while (multiplo < num1){
-        multiplo+=42;
-    }//variavel 'multiplo' vai sair desse loop valendo o primeiro multiplo de 42 do intervalo
-    
-    multiplo+=42; //agora, ela vale o proximo multiplo
-    
-    if(multiplo < num2){
-        return multiplo;
-    }else{
-        console.log("Não encontrado");
-        return false;
+function entreNumeros (min,max){
+    var i= min
+    var f= max
+    while (i<=f){
+        console.log(i);
+        i++
     }
 }
